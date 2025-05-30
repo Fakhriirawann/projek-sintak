f3baa5e5374322eec1775b052165a489a01e311e
RIO <justrio24@gmail.com>
2025-05-30 14:25:27 +0700

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -74,7 +74,7 @@ const Footer = () => {
             <div className="space-y-3">
               <div className="flex items-center space-x-3">
                 <Phone className="w-4 h-4 text-pink-500" />
-                <span className="text-gray-600">+62 812-3456-7890</span>
+                <span className="text-gray-600">+62 895-6213-89403</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-4 h-4 text-pink-500" />
